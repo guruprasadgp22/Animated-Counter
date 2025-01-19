f549db1340c2e3d6fd8d499d87139c756074bd1f
gp <guruprasad26gp@gmail.com>
2025-01-19 19:59:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const counters = document.querySelectorAll(".counter");
+const animationSpeed = 500;
+
+const updateCounter = (counter) => {
+    const targetCount = +counter.getAttribute("data-target");
+    let currentCount = +counter.innerText;
+    const animationStep = targetCount / animationSpeed;
+    if(currentCount < targetCount) {
+        counter.innerText = currentCount + animationStep;setTimeout(()=>{
+            setTimeout(()=>{
+                updateCounter(counter);
+            })
+        },1);
+    } else {
+        counter.innerText = targetCount;
+    }
+}
+
+counters.forEach((counter) => {
+    updateCounter(counter);
+});
